f3f74eef67b8849f7d668e2631a2629bcd668eed
Noxvill <gmo_mmo@live.cl>
2023-11-13 23:39:17 -0300

    cambios varios en js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,44 @@
-precio = 400000
+precio = 300000; 
+var cantidad = 0;
+var total= 0; 
 
+// Mostrando el precio base o inicial del producto
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+
+
+// listener botón + y -
+
+document.querySelector(".btnsumar").addEventListener("click", sumarcantidad);
+document.querySelector(".btnrestar").addEventListener("click", restarcantidad);
+
+
+// Función que se ejecuta cuando se hace click en el botón +
+
+function sumarcantidad(){
+
+    cantidad = cantidad + 1;
+    var q = document.querySelector(".cantidad");
+    q.innerHTML = cantidad
+
+// Mostrando el precio total producto del base * cantidad
+valortotal = document.querySelector(".valor-total");
+valortotal.innerHTML = precio * cantidad;
+
+}
+
+// Función que se ejecuta cuando se hace click en el botón -
+
+function restarcantidad(){
+
+    cantidad = cantidad - 1;
+    var q = document.querySelector(".cantidad");
+    q.innerHTML = cantidad
+
+    // Mostrando el precio total producto del base * cantidad
+valortotal = document.querySelector(".valor-total");
+valortotal.innerHTML = precio * cantidad;
+    
+    }
+
---
+++ assets/js/restarcantidad.js
@@ -0,0 +1,7 @@
+// Función que se ejecuta cuando se hace click en el botón -
+function restarcantidad() {
+    menoscantidadcantidad = document.querySelector(".cantidad");
+    menoscantidad.innerHTML = cantidad - 1;
+
+
+}
